c27352022b4267bad63ab63b3a6b7122289be773
karthik <karthik@cogzytech.com>
2017-08-22 16:55:40 +0530

    Revert "Revert "account class changed""
    This reverts commit b37f3aa72ad550e1dae7feadef1e90d42f471687.

--- gtruckways/src/com/cogzy/gtruckways/businessObjects/Accounts.java
+++ gtruckways/src/com/cogzy/gtruckways/businessObjects/Accounts.java
@@ -2,12 +2,16 @@ package com.cogzy.gtruckways.businessObjects;
 
 public class Accounts {
 
+	
+	// accounts class gtruckways
 	private int accountsId;
 	private String acctType;
 	private String userType;
 	private int addressId;
 	private int billcycleId;
 	private User user;
+	
+	
 	
 	public User getUser() {
 		return user;
